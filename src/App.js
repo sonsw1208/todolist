c9efe37c2f96ee83b6f535095590164fb573588d
sonsw1208 <sonsw12082002@gmail.com>
2023-02-21 23:00:29 +0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">Hello</div>;
+  const [value, setValue] = useState("");
+  return (
+    <div className="flex justify-center items-center h-screen gap-8">
+      <input
+        type="text"
+        className="outline-none border border-blue-600 px-4 py-2 w-[400px]"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-600 rounded-md text-white "
+      >
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default App;
